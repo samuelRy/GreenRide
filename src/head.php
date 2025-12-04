e31b51aba51699df782d66b4027d0ddabe531f6a
Ryan Barkah <elijahsamuelbarkah@gmail.com>
2025-12-04 01:41:11 +0100

    Real small change

--- src/head.php
+++ src/head.php
@@ -2,6 +2,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/style.css">
-    <link rel="icon" type="image/png" href="/icon/scooter-flyer.png">
+    <link rel="icon" type="image/png" href="/icon/scooter-flyer.svg">
     <title>🌱GreenRide</title>
 </head>
